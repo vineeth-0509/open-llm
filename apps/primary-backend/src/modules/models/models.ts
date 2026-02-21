5a8ec73e1cd06b702b583a20d9473837a5839f86
Vineeth-0509 <vineeththungani@gmail.com>
2026-02-22 00:55:51 +0530

    created and done with the models endpoints

---
+++ apps/primary-backend/src/modules/models/models.ts
@@ -0,0 +1,47 @@
+import { t } from "elysia";
+
+export namespace ModelsModel {
+  export const getModelResponseSchema = t.Object({
+    models: t.Array(
+      t.Object({
+        id: t.String(),
+        name: t.String(),
+        slug: t.String(),
+        company: t.Object({
+          id: t.String(),
+          name: t.String(),
+          website: t.String(),
+        }),
+      }),
+    ),
+  });
+  export type getModelResponseSchema = typeof getModelResponseSchema.static;
+
+  export const getProvidersResponseSchema = t.Object({
+    providers: t.Array(
+      t.Object({
+        id: t.String(),
+        name: t.String(),
+        website: t.String(),
+      }),
+    ),
+  });
+  export type getProvidersResponseSchema =
+    typeof getProvidersResponseSchema.static;
+
+  export const getModelProvidersResponseSchema = t.Object({
+    providers: t.Array(
+      t.Object({
+        id: t.String(),
+        providerId: t.String(),
+        providerName: t.String(),
+        providerWebsite: t.String(),
+        inputTokenCost: t.Number(),
+        outputTokenCost: t.Number(),
+      }),
+    ),
+  });
+
+  export type getModelProvidersResponseSchema =
+    typeof getModelProvidersResponseSchema.static;
+}
