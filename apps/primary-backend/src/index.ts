d50357267ba8b04ede48c9bb004ecc421b1100b6
Vineeth-0509 <vineeththungani@gmail.com>
2026-02-22 09:57:21 +0530

    done requesting and testing the models endpoint

--- apps/primary-backend/src/index.ts
+++ apps/primary-backend/src/index.ts
@@ -1,8 +1,14 @@
 import { Elysia } from "elysia";
 import { app as authApp } from "./modules/auth";
 import { app as apiKeys } from "./modules/apikeys";
-
-const app = new Elysia().use(authApp).use(apiKeys).listen(3000);
+import { app as modelsApp } from "./modules/models";
+import { app as paymentApp } from "./modules/payments";
+const app = new Elysia()
+  .use(authApp)
+  .use(apiKeys)
+  .use(modelsApp)
+  .use(paymentApp)
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
